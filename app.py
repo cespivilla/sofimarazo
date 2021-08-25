39d4086fe048c8e6068dd73c42bf69c2972e095b
cespivilla <68255168+cespivilla@users.noreply.github.com>
2021-08-25 00:10:04 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,12 +6,25 @@ from datetime import datetime
 from decouple import config
 from github import Github, InputGitAuthor
 import os
+import requests
 
 # Configure application
 app = Flask(__name__)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
+
+def get_userdata(ip_address):
+    try:
+        response = requests.get("http://ip-api.com/json/{}".format(ip_address))
+        js = response.json()
+        country = js['country']
+        region = js['regionName']
+        latitud = js['lat']
+        longitud = js['lon']
+        return country, region, latitud, longitud
+    except Exception as e:
+        return "Unknown", "Unknown","Unknown","Unknown"
 
 # Usando BD de Melba (tabla users)
 conn = sqlite3.connect('melba.db')
@@ -122,8 +135,33 @@ def instagram(user):
 
 @app.route('/ai1')
 def ai1():
-        # permanecer en la pagina para que usuario revise calmadamente
-        return render_template("ai1.html")
+    fecha = datetime.now()
+    if request.environ.get('HTTP_X_FORWARDED_FOR') is None:
+        ip_address = request.environ['REMOTE_ADDR']
+    else:
+        ip_address = request.environ['HTTP_X_FORWARDED_FOR'] # if behind a proxy
+
+    country, region, latitud, longitud = get_userdata(ip_address)
+
+    mensaje = '{}, {}, {}, {}, {}, {}\n'.format(fecha, ip_adress, country, region, latitud, longitud)
+    token = os.getenv('GITHUB_TOKEN')
+    file_path = "visitas_ai1.txt"
+    g = Github(token)
+    repo = g.get_repo("cespivilla/sofimarazo")
+    file = repo.get_contents(file_path, ref="main")  # Get file from branch
+    data = file.decoded_content.decode("utf-8")  # Get raw string data
+    data += mensaje  # Modify/Create file
+
+    def push(path, message, content, branch, update=False):
+        author = InputGitAuthor("cespivilla","cespivilla@gmail.com")
+        source = repo.get_branch("main")
+        contents = repo.get_contents(path, ref=branch)  # Retrieve old file to get its SHA and path
+        repo.update_file(contents.path, message, content, contents.sha, branch=branch, author=author) 
+    # Add, commit and push branch
+    push(file_path, "Updating visits ai1", data, "main", update=True) 
+    
+    # permanecer en la pagina para que usuario revise calmadamente
+    return render_template("ai1.html")
 
 @app.route('/ai2')
 def ai2():
