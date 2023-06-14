9c4a950b2f1764d3ba2de7901e86e581dfa25e92
cespivilla <68255168+cespivilla@users.noreply.github.com>
2023-06-14 09:50:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -408,36 +408,6 @@ def ai10():
     # permanecer en la pagina para que usuario revise calmadamente
     return render_template("ai10.html")  
 
-@app.route('/ai11')
-def ai11():
-    fecha = datetime.now()
-    if request.environ.get('HTTP_X_FORWARDED_FOR') is None:
-        ip_address = request.environ['REMOTE_ADDR']
-    else:
-        ip_address = request.environ['HTTP_X_FORWARDED_FOR'] # if behind a proxy
-
-    country, region, latitud, longitud = get_userdata(ip_address)
-    if country == "Peru":
-        mensaje = '{}, {}, {}, {}, {}, {}\n'.format(fecha, ip_address, country, region, latitud, longitud)
-        token = os.getenv('GITHUB_TOKEN')
-        file_path = "visitas_ai11.txt"
-        g = Github(token)
-        repo = g.get_repo("cespivilla/sofimarazo")
-        file = repo.get_contents(file_path, ref="main")  # Get file from branch
-        data = file.decoded_content.decode("utf-8")  # Get raw string data
-        data += mensaje  # Modify/Create file
-
-        def push(path, message, content, branch, update=False):
-            author = InputGitAuthor("cespivilla","cespivilla@gmail.com")
-            source = repo.get_branch("main")
-            contents = repo.get_contents(path, ref=branch)  # Retrieve old file to get its SHA and path
-            repo.update_file(contents.path, message, content, contents.sha, branch=branch, author=author) 
-        # Add, commit and push branch
-        push(file_path, "Updating visits ai11", data, "main", update=True) 
-    
-    # permanecer en la pagina para que usuario revise calmadamente
-    return render_template("ai11.html")  
-    
 @app.route('/ai12')
 def ai12():
     fecha = datetime.now()
@@ -483,10 +453,10 @@ def ai16():
         return render_template("ai16.html")
 
 
-# @app.route('/ai18')
-# def ai18():
+@app.route('/ai18')
+def ai18():
         # permanecer en la pagina para que usuario revise calmadamente
-        # return render_template("ai18.html")  
+        return render_template("ai18.html")  
 
 # @app.route('/ai19')
 # def ai19():
